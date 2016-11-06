021fce6a789468914338f11694714d45e280bad6
andrea2107 <andrea210701@gmail.com>
2016-11-06 18:49:47 +0100

    update buildkernel.sh

--- buildkernel.sh
+++ buildkernel.sh
@@ -7,5 +7,6 @@ export CROSS_COMPILE=/home/aarch64-linux-android-4.9-kernel/bin/aarch64-linux-an
 mkdir -p $TOP/KERNEL_OBJ
 
 make O=$TOP/KERNEL_OBJ ARCH=arm64 MTK_TARGET_PROJECT=hermes TARGET_BUILD_VARIANT=user CROSS_COMPILE=$TOOLCHAIN ROOTDIR=$TOP hermes_defconfig
+make mrproper
 make O=$TOP/KERNEL_OBJ ROOTDIR=$TOP
 
