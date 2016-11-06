b85b126081162b4ccb0df11ceab326c4c2f0c9cb
andrea2107 <andrea210701@gmail.com>
2016-11-06 19:07:27 +0100

    Update mm.h

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1632,11 +1632,8 @@ int write_one_page(struct page *page, int wait);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-<<<<<<< HEAD
 #define VM_MAX_READAHEAD	128	/* kbytes */
-=======
 #define VM_MAX_READAHEAD	512	/* kbytes */
->>>>>>> 36b5ad3... mm: reduce readahead from 1024kB to 512kB
 #define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
