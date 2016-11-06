8096e78210a977b3ec39b78fdb211f33adb369df
andrea2107 <andrea210701@gmail.com>
2016-11-06 17:07:16 +0100

    Update buildkernel.sh

--- buildkernel.sh
+++ buildkernel.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-#alternative working tc: aarch64-linux-android-4.9-kernel
-#alternative working tc: android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9
 
-export ARCH=arm64
-export CONFIG_NO_ERROR_ON_MISMATCH=y
-export CROSS_COMPILE=aarch64-linux-android-
-export PATH=/home/osboxes/toolchains/aarch64-linux-android-4.9_unmodded/bin:$PATH
-#export CONFIG_DEBUG_SECTION_MISMATCH=y
+export TOP=$(pwd)
+export CROSS_COMPILE=/home/aarch64-linux-android-4.9-kernel/bin/aarch64-linux-android-
 
-find ./ -name '*~' -o -name '*.bak' | xargs rm;
-rm -rf ~/.ccache;
-make distclean -w V=0 && make mrproper -w V=0;
-echo "Compiling Kernel, using config file $1, this might take long, window will be closed after it is done...";
-echo "You will find errors and warnings of kernel compilation in build_errors_warnings.log file.";
-rm ./.config;
-nice make -w V=0 $1 2> ./build_errors_warnings.log;
-make -w -j`grep 'processor' /proc/cpuinfo | wc -l` V=0 2>> ./build_errors_warnings.log;
+mkdir -p $TOP/KERNEL_OBJ
+
+make O=$TOP/KERNEL_OBJ ARCH=arm64 MTK_TARGET_PROJECT=hermes TARGET_BUILD_VARIANT=user CROSS_COMPILE=$TOOLCHAIN ROOTDIR=$TOP hermes_defconfig
+make O=$TOP/KERNEL_OBJ ROOTDIR=$TOP
 
