a2af9394f712aa4e9dd3fb771bc7f1343317e891
andrea2107 <andrea210701@gmail.com>
2016-11-07 16:39:08 +0100

    Update row-iosched.c

--- block/row-iosched.c
+++ block/row-iosched.c
@@ -96,45 +96,7 @@ static const struct row_queue_params row_queues_def[] = {
 };
 
 /* Default values for idling on read queues (in msec) */
-<<<<<<< HEAD
-<<<<<<< HEAD
-#define ROW_IDLE_TIME_MSEC 5
-#define ROW_READ_FREQ_MSEC 5
-=======
-#define ROW_IDLE_TIME_MSEC 10
-#define ROW_READ_FREQ_MSEC 25
-=======
-/* Flags indicating whether idling is enabled on the queue */
-static const bool queue_idling_enabled[] = {
-	true,	/* ROWQ_PRIO_HIGH_READ */
-	true,	/* ROWQ_PRIO_REG_READ */
-	false,	/* ROWQ_PRIO_HIGH_SWRITE */
-	false,	/* ROWQ_PRIO_REG_SWRITE */
-	false,	/* ROWQ_PRIO_REG_WRITE */
-	false,	/* ROWQ_PRIO_LOW_READ */
-	false,	/* ROWQ_PRIO_LOW_SWRITE */
-};
 
-/* Default values for row queues quantums in each dispatch cycle */
-static const int queue_quantum[] = {
-	100,	/* ROWQ_PRIO_HIGH_READ */
-	100,	/* ROWQ_PRIO_REG_READ */
-	2,	/* ROWQ_PRIO_HIGH_SWRITE */
-	1,	/* ROWQ_PRIO_REG_SWRITE */
-	1,	/* ROWQ_PRIO_REG_WRITE */
-	1,	/* ROWQ_PRIO_LOW_READ */
-	1	/* ROWQ_PRIO_LOW_SWRITE */
-};
-
-/* Default values for idling on read queues */
-#define ROW_IDLE_TIME_MSEC 5	/* msec */
-#define ROW_READ_FREQ_MSEC 20	/* msec */
->>>>>>> 274539c... Add ROW I/O Scheduler by Qualcomm.
->>>>>>> 1f9c78b... Add ROW I/O Scheduler by Qualcomm.
-=======
-#define ROW_IDLE_TIME_MSEC 5
-#define ROW_READ_FREQ_MSEC 5
->>>>>>> 2b190cf... Update row-iosched.c
 
 /**
  * struct rowq_idling_data -  parameters for idling on the queue
