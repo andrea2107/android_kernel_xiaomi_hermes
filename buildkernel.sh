834987b8ba06f98851f7a19492c50e1e4371d329
andrea2107 <andrea210701@gmail.com>
2016-11-09 14:54:21 +0100

    Update buildkernel.sh

--- buildkernel.sh
+++ buildkernel.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+export ARCH=arm64
+export CONFIG_NO_ERROR_ON_MISMATCH=y
+export CROSS_COMPILE=aarch64-linux-android-
+export PATH=/home/aarch64-linux-android-4.9-kernel/bin:$PATH
+#export CONFIG_DEBUG_SECTION_MISMATCH=y
 
-export TOP=$(pwd)
-export CROSS_COMPILE=/home/aarch64-linux-android-4.9-kernel/bin/aarch64-linux-android-
-
-mkdir -p $TOP/KERNEL_OBJ
-make O=$TOP/KERNEL_OBJ ARCH=arm64 MTK_TARGET_PROJECT=hermes TARGET_BUILD_VARIANT=user CROSS_COMPILE=$TOOLCHAIN ROOTDIR=$TOP hermes_defconfig
-make mrproper
-make O=$TOP/KERNEL_OBJ ROOTDIR=$TOP
-
+find ./ -name '*~' -o -name '*.bak' | xargs rm;
+rm -rf ~/.ccache;
+make distclean -w V=0 && make mrproper -w V=0;
+echo "Compiling Kernel, using config file $1, this might take long, window will be closed after it is done...";
+echo "You will find errors and warnings of kernel compilation in build_errors_warnings.log file.";
+rm ./.config;
+nice make -w V=0 MTK_TARGET_PROJECT=hermes TARGET_BUILD_VARIANT=user $1 2> ./build_errors_warnings.log;
+make -w -j`grep 'processor' /proc/cpuinfo | wc -l` V=0 2>> ./build_errors_warnings.log;
