52b64997057a8dd8e92c5632a2bc879e51820847
andrea2107 <andrea210701@gmail.com>
2016-11-09 16:50:35 +0100

    Update focaltech_driver.c

--- drivers/input/touchscreen/mediatek/ft5206/focaltech_driver.c
+++ drivers/input/touchscreen/mediatek/ft5206/focaltech_driver.c
@@ -46,6 +46,9 @@ extern int tpd_v_magnify_y;
 #include <linux/delay.h>
 #include "tpd_custom_fts.h"
 #include "focaltech_ex_fun.h"
+#ifdef CONFIG_WAKE_GESTURES
+#include <linux/wake_gestures.h>
+#endif
 
 u8 *I2CDMABuf_va = NULL;
 dma_addr_t I2CDMABuf_pa = 0;
